e79fa1095564718b80ff2db9bbbe79371071d2bc
Matthew Clark <afromatt6288@hotmail.com>
2023-02-16 13:56:11 -0700

    Lab completed... barely

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentance) =>{
+    const word = sentance.split(" ")
+    const capWords = word.map((word) =>  
+      word[0].toUpperCase() + word.substring(1) 
+    )
+    const newSentance = capWords.join(" ");
+    console.log(newSentance)
+    return newSentance
+  })
 }
+titleCased()
